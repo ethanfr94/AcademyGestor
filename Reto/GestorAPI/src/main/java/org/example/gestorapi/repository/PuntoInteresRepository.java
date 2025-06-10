89c784fc8db312d8bc62703ad5f83e3ced577c1c
ethanfr94 <152906243+ethanfr94@users.noreply.github.com>
2025-06-10 22:21:31 +0200

    .

--- Reto/GestorAPI/src/main/java/org/example/gestorapi/repository/PuntoInteresRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.gestorapi.repository;
-
-import org.example.gestorapi.model.PuntoInteres;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PuntoInteresRepository extends JpaRepository<PuntoInteres, Integer> {
-}
